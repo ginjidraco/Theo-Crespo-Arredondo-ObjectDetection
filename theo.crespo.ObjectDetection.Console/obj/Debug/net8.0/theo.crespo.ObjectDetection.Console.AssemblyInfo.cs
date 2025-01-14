d0a9ea396abe9920dcfb36e855d603f289b9b268
ThéophileCrespoArred <theophile.crespo@gmail.com>
2025-01-14 11:47:04 +0100

    first

--- theo.crespo.ObjectDetection.Console/obj/Debug/net8.0/theo.crespo.ObjectDetection.Console.AssemblyInfo.cs
+++ theo.crespo.ObjectDetection.Console/obj/Debug/net8.0/theo.crespo.ObjectDetection.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("theo.crespo.ObjectDetection.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfb509e0b2a3a94345ad17263df7ececd8a8a44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("theo.crespo.ObjectDetection.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("theo.crespo.ObjectDetection.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
